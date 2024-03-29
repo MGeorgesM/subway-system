1d1ae308a2a48e5166e05ac076082cb4d202c8be
George <g.m.6@live.com>
2024-03-29 02:15:51 +0200

    ssb-6 role model seed

---
+++ api/database/seeders/RolesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role = new Role();
+        $role->name = 'user';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'branch';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'headquarter';
+        $role->save();
+    }
+}
