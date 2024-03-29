d95b39c4cd27e5cbec83c3aba2b04e8c53c8499b
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-03-29 23:42:28 +0200

    CoinRequest APIs

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\CoinRequestController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -29,3 +29,8 @@ Route::post('auth/logout', [AuthController::class, 'logout']);
 Route::get('users/get/{id?}', [UsersController::class, 'getUsers']);
 Route::post('users/update', [UsersController::class, 'updateUser']);
 
+Route::post('/coins-requests', [CoinRequestController::class, 'requestCoin']);
+Route::get('/coins-requests', [CoinRequestController::class, 'viewRequests']);
+Route::post('/coins-requests/{id}/accept', [CoinRequestController::class, 'acceptRequest']);
+Route::post('/coins-requests/{id}/discard', [CoinRequestController::class, 'discardRequest']);
+Route::get('/user/{userId}/coins-requests', [CoinRequestController::class, 'userRequests']);
