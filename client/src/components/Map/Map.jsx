75988d5454b6f86bcabf37863231986eb4061890
George <g.m.6@live.com>
2024-03-30 00:55:37 +0200

    ssb-12-map location search

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 
 import './index.css';
@@ -7,34 +7,17 @@ import { Icon } from 'leaflet';
 
 const Map = () => {
     const [userLocation, setUserLocation] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
 
-    const LocationMarker = () => {
-        useMapEvents({
-            click(e) {
-                // map.locate();
-                console.log(e.latlng);
-                setUserLocation(e.latlng);
-            },
-        });
+    // useEffect(() => {
+    //     if(userLocation && map) {
+    //         map.flyTo(userLocation, map.getZoom());
+    //     }
+    // })
 
-        // const map = useMapEvents({
-        //     click(e) {
-        //         map.locate();
-        //         console.log(e.latlng)
-        //         setUserLocation(e.latlng);
-        //     },
-        // locationfound(e) {
-        //     setUserLocation(e.latlng);
-        //     map.flyTo(e.latlng, map.getZoom());
-        // },
-        // });
-
-        return userLocation === null ? null : (
-            <Marker position={userLocation} icon={customUserIcon}>
-                <Popup>You are here</Popup>
-            </Marker>
-        );
-    };
+    useEffect(() => {
+        console.log('userLocation', userLocation);
+    }, [userLocation]);
 
     const stations = [
         {
@@ -59,6 +42,71 @@ const Map = () => {
         },
     ];
 
+    const LocationMarker = () => {
+        const map = useMapEvents({
+            click(e) {
+                // map.locate();
+                const { lat, lng } = e.latlng;
+                map.flyTo([lat, lng], map.getZoom());
+                setUserLocation([lat, lng]);
+            },
+
+        });
+        
+        useEffect(() => {
+            console.log('running')
+            if (userLocation && map) {
+                map.flyTo(userLocation, map.getZoom());
+            }
+        }, [map]);
+
+
+        // const map = useMapEvents({
+        //     click(e) {
+        //         map.locate();
+        //         console.log(e.latlng)
+        //         setUserLocation(e.latlng);
+        //     },
+        // locationfound(e) {
+        //     setUserLocation(e.latlng);
+        //     map.flyTo(e.latlng, map.getZoom());
+        // },
+        // });
+
+        return userLocation === null ? null : (
+            <Marker position={userLocation} icon={customUserIcon}>
+                <Popup>You are here</Popup>
+            </Marker>
+        );
+    };
+
+    // const handleMapMove = () => {
+    //     useMapEvents({
+    //         flyTo(userLocation, map.getZoom()),
+    //     });
+    // };
+
+    const handleSearch = async () => {
+        const apiKey = '660737a1376dd241495384iohcb525f';
+        console.log('searchquery', searchQuery);
+        const response = await fetch(`https://geocode.maps.co/search?q=${searchQuery}&api_key=${apiKey}`);
+        const data = await response.json();
+
+        console.log('data', data);
+
+        if (data.length > 0) {
+            const { lat, lon } = data[0];
+            setUserLocation([parseFloat(lat), parseFloat(lon)]);
+        } else {
+            console.log('No data found');
+        }
+
+    };
+
+    const handleInputChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
     const customUserIcon = new Icon({
         iconUrl: 'https://cdn-icons-png.flaticon.com/512/2776/2776067.png',
         iconSize: [50, 50],
@@ -66,21 +114,27 @@ const Map = () => {
 
     const customStationIcon = new Icon({
         iconUrl: 'https://cdn-icons-png.flaticon.com/512/6571/6571498.png',
-        iconSize: [50, 50],
+        iconSize: [32, 32],
     });
 
     return (
-        <MapContainer center={[33.88863, 35.49548]} zoom={13}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <TileLayer url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png" />
+        <div className="map-container">
+            <div className="searh=container">
+                <input type="text" value={searchQuery} onChange={handleInputChange} placeholder="Enter your city" />
+                <button onClick={handleSearch}>Search</button>
+            </div>
+            <MapContainer center={userLocation ? userLocation : [33.88863, 35.49548]} zoom={13}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <TileLayer url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png" />
 
-            {stations.map((station) => (
-                <Marker position={station.location} key={station.name} icon={customStationIcon}>
-                    <Popup>{station.name}</Popup>
-                </Marker>
-            ))}
-            <LocationMarker />
-        </MapContainer>
+                {stations.map((station) => (
+                    <Marker position={station.location} key={station.name} icon={customStationIcon}>
+                        <Popup>{station.name}</Popup>
+                    </Marker>
+                ))}
+                <LocationMarker />
+            </MapContainer>
+        </div>
     );
 };
 
