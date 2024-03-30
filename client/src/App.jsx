6eff2504f8f3b739c3af87df2255e44bf6611a94
George <g.m.6@live.com>
2024-03-30 22:21:17 +0200

    ssb-14-initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,10 @@ import './styles/colors.css'
 import './styles/utilities.css'
 import './styles/queries.css'
 
-import Map from './components/Map/Map'
-
 function App() {
   return (
     <div className="App">
-      <Map />
+
     </div>
   );
 }
