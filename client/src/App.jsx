9e5d5e10702917752190898682e011dac6b3664b
George <g.m.6@live.com>
2024-03-31 19:31:21 +0300

    app.jsx cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Authentication from './components/Authentication/Authentication';
+
 import './App.css';
 import './styles/colors.css'
 import './styles/utilities.css'
 import './styles/queries.css'
-
-import Map from './components/Map/Map'
-
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Authentication from './components/Authentication/Authentication';
-
-import './App.css';
-import './styles/colors.css';
-import './styles/utilities.css';
-import './styles/queries.css';
 
 function App() {
     return (
