6e89e7e945f2acc9326137e58af64eb7564d9415
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-04-01 17:49:19 +0300

    added switcher between reviews and messages

--- client/src/components/Authentication/Authentication.jsx
+++ client/src/components/Authentication/Authentication.jsx
@@ -1,90 +1,102 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { sendRequest } from '../../core/tools/apiRequest';
-import { requestMethods } from '../../core/tools/apiRequestMethods';
+import { sendRequest } from "../../core/tools/apiRequest";
+import { requestMethods } from "../../core/tools/apiRequestMethods";
 
-import SignInForm from './Forms/SignInForm';
-import SignUpForm from './Forms/SignUpForm';
+import SignInForm from "./Forms/SignInForm";
+import SignUpForm from "./Forms/SignUpForm";
 
-import './index.css';
+import "./index.css";
 
 const Authentication = () => {
-    const [isLogin, setIsLogin] = useState(true);
-    const [error, setError] = useState('');
-    const [formData, setFormData] = useState({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-    });
+  const [isLogin, setIsLogin] = useState(true);
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+  });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!formData.email.includes('@') && formData.email.length > 0) {
-            setError('Invalid email');
-        } else if (formData.password.length < 8 && formData.password.length > 0) {
-            setError('Password must be at least 8 characters');
-        } else {
-            setError('');
-        }
-    }, [formData]);
+  useEffect(() => {
+    if (!formData.email.includes("@") && formData.email.length > 0) {
+      setError("Invalid email");
+    } else if (formData.password.length < 8 && formData.password.length > 0) {
+      setError("Password must be at least 8 characters");
+    } else {
+      setError("");
+    }
+  }, [formData]);
 
-    const switchHandler = (isLogin) => {
-        setIsLogin(isLogin);
-    };
+  const switchHandler = (isLogin) => {
+    setIsLogin(isLogin);
+  };
 
-    const handleLogin = async (formData) => {
-        const data = new FormData();
-        data.append('email', formData.email);
-        data.append('password', formData.password);
+  const handleLogin = async (formData) => {
+    const data = new FormData();
+    data.append("email", formData.email);
+    data.append("password", formData.password);
 
-        try {
-            const response = await sendRequest(requestMethods.POST, '/auth/login', data);
-            if (response.status === 200) {
-                localStorage.setItem('token', JSON.stringify(response.data.token));
-                navigate('/');
-                return;
-            } else {
-                throw new Error('Wrong email or password');
-            }
-        } catch (error) {
-            console.log(error.message);
-            setError([error.message]);
-        }
-    };
+    try {
+      const response = await sendRequest(
+        requestMethods.POST,
+        "/auth/login",
+        data
+      );
+      if (response.status === 200) {
+        localStorage.setItem("token", JSON.stringify(response.data.token));
+        navigate("/");
+        return;
+      } else {
+        throw new Error("Wrong email or password");
+      }
+    } catch (error) {
+      console.log(error.message);
+      setError([error.message]);
+    }
+  };
 
-    const handleSignup = async (formData) => {
-        try {
-            const response = await sendRequest(requestMethods.POST, '/auth/register', formData);
-            if (response.status === 201) {
-                localStorage.setItem('token', JSON.stringify(response.data.token));
-                navigate('/location');
-                return;
-            } else {
-                throw new Error();
-            }
-        } catch (error) {
-            setError([error.response.data.message]);
-        }
-    };
+  const handleSignup = async (formData) => {
+    try {
+      const response = await sendRequest(
+        requestMethods.POST,
+        "/auth/register",
+        formData
+      );
+      if (response.status === 201) {
+        localStorage.setItem("token", JSON.stringify(response.data.token));
+        navigate("/location");
+        return;
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      setError([error.response.data.message]);
+    }
+  };
 
-    return (
-        <section className="form-component flex center">
-            {isLogin ? (
-                <SignInForm switchHandler={switchHandler} handleLogin={handleLogin} error={error} />
-            ) : (
-                <SignUpForm
-                    switchHandler={switchHandler}
-                    handleSignup={handleSignup}
-                    setFormData={setFormData}
-                    formData={formData}
-                    error={error}
-                />
-            )}
-        </section>
-    );
+  return (
+    <section className="form-component flex center">
+      {isLogin ? (
+        <SignInForm
+          switchHandler={switchHandler}
+          handleLogin={handleLogin}
+          error={error}
+        />
+      ) : (
+        <SignUpForm
+          switchHandler={switchHandler}
+          handleSignup={handleSignup}
+          setFormData={setFormData}
+          formData={formData}
+          error={error}
+        />
+      )}
+    </section>
+  );
 };
 
 export default Authentication;
--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./profile.css";
 import userImage from "../../assets/user-image.jpeg";
 import { MdEdit } from "react-icons/md";
+import { FaStar, FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+import { sendRequest } from "../../core/tools/apiRequest";
+import { requestMethods } from "../../core/tools/apiRequestMethods";
 
 function Profile() {
+  const [user, setUser] = useState("");
+  const [displayContent, setDisplayContent] = useState("user-reviews");
+
+  useEffect(() => {
+    getUserInfo();
+    console.log(user);
+  }, []);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await sendRequest(requestMethods.GET, `/users/get/4`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+      const data = await response.json();
+      setUser(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  function ReviewCard({ name, stationName, review }) {
+    return (
+      <div className="review-card">
+        <img src={userImage} alt="User" />
+        <p>{name}</p>
+        <p>{stationName}</p>
+        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+        <p>{review}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="profile-wrapper">
       <div className="profile-header">
@@ -44,6 +81,64 @@ function Profile() {
             </p>
             <button className="general-btn">Request Coins</button>
           </div>
+        </div>
+      </div>
+
+      <div className="user-reviews-wrapper">
+        <div className="button-switcher">
+          <h1 onClick={() => setDisplayContent("user-reviews")}>
+            User Reviews
+          </h1>
+          <h1 onClick={() => setDisplayContent("admin-messages")}>
+            Admin Messages
+          </h1>
+        </div>
+
+        {displayContent === "user-reviews" && (
+          <div className="reviews-cards-wrapper">
+            <ReviewCard
+              name="John Doe"
+              stationName="Station A"
+              review="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+          sed aperiam facilis magnam itaque ea rem tenetur veritatis quidem
+          eligendi! Velit aperiam totam veritatis dignissimos quo! Dignissimos
+          ex asperiores cupiditate."
+            />
+            <ReviewCard
+              name="John Doe"
+              stationName="Station B"
+              review="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+          sed aperiam facilis magnam itaque ea rem tenetur veritatis quidem
+          eligendi! Velit aperiam totam veritatis dignissimos quo! Dignissimos
+          ex asperiores cupiditate."
+            />
+            <ReviewCard
+              name="John Doe"
+              stationName="Station C"
+              review="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+          sed aperiam facilis magnam itaque ea rem tenetur veritatis quidem
+          eligendi! Velit aperiam totam veritatis dignissimos quo! Dignissimos
+          ex asperiores cupiditate."
+            />
+          </div>
+        )}
+        {displayContent === "admin-messages" && (
+          <div className="admin-messages-wrapper">
+            <p>Admin Messages Content Goes Here</p>
+          </div>
+        )}
+      </div>
+
+      <div className="profile-footer">
+        <div className="footer-icons">
+          <FaTwitter className="icon" />
+          <FaFacebook className="icon" />
+          <FaInstagram className="icon" />
+          <p>Subway System App - SE Factory - April 2024</p>
+        </div>
+        <div className="right-footer">
+          <p>Careers</p>
+          <p>About Us</p>
         </div>
       </div>
     </div>
