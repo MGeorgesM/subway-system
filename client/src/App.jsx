429628bc703fbb91d7ecf44909b277b2cd22c196
George <g.m.6@live.com>
2024-04-01 11:49:26 +0300

    ssb-23-navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar/Navbar';
 import Authentication from './components/Authentication/Authentication';
 import LocationForm from './components/Authentication/Forms/LocationForm';
-import Profile from "./components/profile/profile";
+import Home from './components/Home/Home';
+import Profile from './components/profile/profile';
 
 import './App.css';
 import './styles/colors.css';
@@ -12,9 +14,11 @@ import './styles/queries.css';
 function App() {
     return (
         <BrowserRouter>
+        <Navbar />
             <Routes>
+                {/* <Route path="/" element={<Home />} /> */}
                 <Route path="/auth" element={<Authentication />} />
-                <Route path ="/location" element={<LocationForm />} />
+                <Route path="/location" element={<LocationForm />} />
                 <Route path="/profile" element={<Profile />} />
             </Routes>
         </BrowserRouter>
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './index.css';
+
+const Navbar = ({ bg = 'black-bg' }) => {
+    return (
+        <section className={`navbar ${bg}`}>
+            <nav className="nav-elements flex space-between light">
+                <img className="logo" alt="logo" src="./images/Assets/logo.png" />
+                <div>
+                    <div className="nav-list flex center">
+                        <button className="nav-link white-text no-bg">About Us</button>
+                        <button className="nav-login white-text primary-bg box-shadow border-radius regular">
+                            Sign In
+                        </button>
+                    </div>
+                </div>
+            </nav>
+        </section>
+    );
+};
+
+export default Navbar;
