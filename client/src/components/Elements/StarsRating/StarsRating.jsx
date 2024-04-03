8abb58333d293bd1a7386598c85cb8875dfea0fa
George <g.m.6@live.com>
2024-04-03 20:05:01 +0300

    ssb-24-rating

--- client/src/components/Elements/StarsRating/StarsRating.jsx
+++ client/src/components/Elements/StarsRating/StarsRating.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
-const StarsRating = ({rating}) => {
-    return <FontAwesomeIcon icon={faStar} color="#6D6B6C" />;
+const StarsRating = ({ rating }) => {
+    const filledStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+    const stars = [];
+
+    for (let i = 0; i < filledStars; i++) {
+        stars.push(<FontAwesomeIcon key={i} icon={solidStar} color="#ED941B" />);
+    }
+    if (hasHalfStar) {
+        stars.push(<FontAwesomeIcon key="half-star" icon={solidStar} color="#ED941B" half />);
+    }
+    const remainingStars = 5 - stars.length;
+    for (let i = 0; i < remainingStars; i++) {
+        stars.push(<FontAwesomeIcon key={`empty-${i}`} icon={regularStar} color="#6D6B6C" />);
+    }
+
+    return <>{stars}</>;
 };
 
 export default StarsRating;
