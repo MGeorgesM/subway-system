618792b5adca7947b4e339ca29c877a54606fb27
George <g.m.6@live.com>
2024-04-04 01:41:40 +0300

    ssb-29-ticketing

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
+import Button from '../Elements/Button/Button';
 
 import './index.css';
-import Button from '../Elements/Button/Button';
 
 const Welcome = () => {
     const navigate = useNavigate();
@@ -10,20 +10,16 @@ const Welcome = () => {
         <div className="main-welcome flex column center">
             <div className="welcome-text flex column center">
                 <h1>Welcome to Onward</h1>
-                <p>
-                    Your gateway to seamless urban transit, explore with ease as you embark on
-                    your journey with us.
-                </p>
+                <p>Your gateway to seamless urban transit, explore with ease as you embark on your journey with us.</p>
             </div>
             <div className="welcome-btns flex center">
                 <Button clickHandler={() => navigate('/auth')} text={'Sign In'} type={'primary-btn'} size={'btn-l'} />
-                <Button clickHandler={() => navigate('/browse')} text={'Browse'} type={'secondary-btn'} size={'btn-l'} />
-                {/* <button className="primary-btn border-radius-m" onClick={() => navigate('/auth')}>
-                    Sign In
-                </button>
-                <button className="clicked secondary border-radius-m bold" onClick={() => navigate('/browse')}>
-                    Browse
-                </button> */}
+                <Button
+                    clickHandler={() => navigate('/browse')}
+                    text={'Browse'}
+                    type={'secondary-btn'}
+                    size={'btn-l'}
+                />
             </div>
         </div>
     );
