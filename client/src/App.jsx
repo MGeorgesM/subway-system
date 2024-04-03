cc41bfd838b79a83f40d75235b958224f69896b2
George <g.m.6@live.com>
2024-04-03 23:25:04 +0300

    ssb-29-auth-clean

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Elements/Navbar/Navbar';
 import Welcome from './components/Welcome/Welcome';
-import Footer from './components/Footer/Footer';
+import Footer from './components/Elements/Footer/Footer';
 import Home from './components/Home/Home';
 import Station from './components/Station/Station';
 import Profile from './components/Profile/Profile';
--- client/src/components/Elements/Navbar/logic.js
+++ client/src/components/Elements/Navbar/logic.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { sendRequest } from '../../core/tools/apiRequest';
-import { requestMethods } from '../../core/tools/apiRequestMethods';
+import { sendRequest } from '../../../core/tools/apiRequest';
+import { requestMethods } from '../../../core/tools/apiRequestMethods';
 
 export const useNavBarLogic = () => {
     const [scrolled, setScrolled] = useState(false);
