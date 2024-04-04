0518aee6a424f71bba98a713e8c5a94f57b8df4d
George <g.m.6@live.com>
2024-04-04 19:02:11 +0300

    ssb-36-initial

--- client/src/components/Ticket/Ticket.jsx
+++ client/src/components/Ticket/Ticket.jsx
@@ -1,3 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
+import { sendRequest } from '../../core/tools/apiRequest';
+import { requestMethods } from '../../core/tools/apiRequestMethods';
+
+
 import { useTicketLogic } from './logic';
 
 import Popup from '../Elements/Popup/Popup';
@@ -8,24 +15,113 @@ import { formatTime } from '../../core/tools/formatTime';
 import './index.css';
 
 const Ticket = () => {
-    const {
-        user,
-        ride,
-        count,
-        popupMessage,
-        showPopup,
-        navigate,
-        handleCancel,
-        handleChange,
-        handleProceed,
-        handleCheckout,
-        handleDecrement,
-        handleIncrement,
-    } = useTicketLogic();
+    // const {
+    //     user,
+    //     ride,
+    //     count,
+    //     popupMessage,
+    //     showPopup,
+    //     passSelelected,
+    //     navigate,
+    //     handleCancel,
+    //     handleChange,
+    //     handleProceed,
+    //     handleCheckout,
+    //     handleDecrement,
+    //     handleIncrement,
+    //     handlePassSelection,
+    // } = useTicketLogic();
+
+    const [count, setCount] = useState(1);
+    const [ride, setRide] = useState({});
+    const [user, setUser] = useState({});
+
+    const [popupMessage, setPopupMessage] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
+
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+
+    const rideId = parseInt(searchParams.get('rideid'));
+    const stationId = parseInt(searchParams.get('stationid'));
+
+    useEffect(() => {
+        const getRide = async () => {
+            try {
+                const response = await sendRequest(requestMethods.GET, `rides/get/${rideId}`, null);
+                if (response.status === 200) {
+                    setRide(response.data.rides);
+                } else {
+                    throw new Error();
+                }
+            } catch (error) {
+                console.log(error.response.data.message);
+            }
+        };
+        const getUser = async () => {
+            try {
+                const response = await sendRequest(requestMethods.GET, '/users/get', null);
+                if (response.status === 200) {
+                    setUser(response.data.user);
+                } else {
+                    throw new Error();
+                }
+            } catch (error) {
+                console.log(error.response.data.message);
+            }
+        };
+        getUser();
+        getRide();
+    }, [rideId]);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+
+    const handleDecrement = () => {
+        if (count > 1) {
+            setCount(count - 1);
+        }
+    };
+
+    const handleChange = (e) => {
+        const value = parseInt(e.target.value);
+        if (!isNaN(value) && value >= 1) {
+            setCount(value);
+        }
+    };
+
+    const handleCheckout = async () => {
+        try {
+            const response = await sendRequest(requestMethods.POST, 'tickets/create', { rideId, count });
+            if (response.status === 201) {
+                setShowPopup(true);
+                setPopupMessage('Your Purchase was successfull');
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            setShowPopup(true);
+            setPopupMessage(`${error.response.data.message}`);
+        }
+    };
+
+    const handleCancel = () => {
+        navigate(`/station?id=${stationId}`);
+    };
+
+    const handleProceed = () => {
+        setShowPopup(false);
+    };
+
+    const handlePassSelection = () => {
+        
+
 
     if (user && ride)
         return (
             <>
+                {/* <div className='flex center'> */}
                 <div className="ticket-main flex center">
                     <div className="ride-details-container flex column center border-radius box-shadow">
                         <div className="ride-details-title dark-text flex center column">
@@ -71,9 +167,13 @@ const Ticket = () => {
                             />
                             <Button text={'Cancel'} type={'secondary-btn'} size={'btn-s'} clickHandler={handleCancel} />
                         </div>
+                        <div className="pass-prompt bold secondary-text">
+                           <p onClick={handlePassSelection}>Get a Multi-Ride Pass and Start Saving!</p>
+                        </div>
                     </div>
                 </div>
                 {showPopup && <Popup message={popupMessage} handleContinue={handleProceed} />}
+                {/* </div> */}
             </>
         );
 };
